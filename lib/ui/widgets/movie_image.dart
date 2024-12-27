69510974e6ec6dc491d981a107a62d992d4f0656
imeujin <imeujin.dev@gmail.com>
2024-12-27 17:13:21 +0900

    [feat] 이미지 누르면 detail 페이지로 이동

--- lib/ui/widgets/movie_image.dart
+++ lib/ui/widgets/movie_image.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_info_app/ui/detail/detail_page.dart';
 
 class MovieImage extends StatelessWidget {
   MovieImage({
@@ -13,14 +14,26 @@ class MovieImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: height,
-      width: width,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Image.network(
-          url,
-          fit: BoxFit.fill,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return DetailPage();
+            },
+          ),
+        );
+      },
+      child: SizedBox(
+        height: height,
+        width: width,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.network(
+            url,
+            fit: BoxFit.fill,
+          ),
         ),
       ),
     );
