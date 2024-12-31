7d4ee2de2dc1713507c3914af6aa568f36287fe4
imeujin <imeujin.dev@gmail.com>
2024-12-31 11:32:50 +0900

    [fix] 제작사 로고 없을 경우 이름으로 대체해서 이미지 출력하도록 개선

--- lib/presentation/detail/widgets/detail_production_company.dart
+++ lib/presentation/detail/widgets/detail_production_company.dart
@@ -14,12 +14,30 @@ class DetailProductionCompany extends StatelessWidget {
         itemCount: productCompanies.length,
         separatorBuilder: (context, index) => const SizedBox(width: 10),
         itemBuilder: (context, index) {
-          return SizedBox(
+          final path = productCompanies[index].logoPath;
+
+          return Container(
             width: 140,
-            child: Image.network(
-              productCompanies[index].logoPath!,
-              fit: BoxFit.cover,
+            // center 로 지정하면 container 사이즈에 맞게 이미지가 잘 맞춰서 들어감
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              // 투명도 0.9 지정
+              color: Colors.white.withOpacity(0.9),
             ),
+            // path 가 null 이라 이미지를 못 가져오면 제작사 이름 출력
+            child: path!.endsWith('null')
+                ? Text(
+                    textAlign: TextAlign.center,
+                    productCompanies[index].name,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                : Image.network(
+                    path,
+                    fit: BoxFit.cover,
+                  ),
           );
         },
       ),
